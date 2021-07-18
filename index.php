525ebfb00b0421900896f241677083e57f804b77
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-18 07:43:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -40,11 +40,11 @@
 
 <!-- Error Bar -->
 <?php
-if(isset($error) && $error !== ""){
-    echo '<h6 class = "error_text">';
+///if(isset($error) && $error !== ""){
+    //echo '<h6 class = "error_text">';
 	//echo "" . htmlspecialchars($error) . "";
-	echo '</h6>'
-}
+	//echo '</h6>'
+//}
 ?>
 
 <!-- Introduction -->
