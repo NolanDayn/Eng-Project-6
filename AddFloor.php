ecfc0c79f07466122cd60003d8061260392e5df6
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-18 13:40:56 -0400

    Update AddFloor.php

--- AddFloor.php
+++ AddFloor.php
@@ -7,7 +7,7 @@ $floor = $_GET["floor"];
 if($_SERVER["REQUEST_METHOD"] == "GET"){
 
         //$con = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
-        
+        /*
         if($link->connection_error){
             die("Connection failed: " . $link->connection_error);
         }
@@ -21,27 +21,27 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
         }
 
         $link->close();
-
+		*/
         //Insert a new requested floor to the elevators database
-        //$stmt = $mysqli->prepare("INSERT INTO requests(floor) VALUE(?)");    
-       // $stmt = mysqli_prepare($link, $sql)){
+        $stmt = $mysqli->prepare('INSERT INTO `requests` (`requestNumber`, `floor`) VALUES (NULL, ?)');    
+		$stmt = mysqli_prepare($link, $sql)){
         
             // Bind variables to the prepared statement as parameters
-       // mysqli_stmt_bind_param("is", $floor);
+		mysqli_stmt_bind_param($stmt, "s", $floor);
         
 
         // Attempt to execute the prepared statement
-       // if(mysqli_stmt_execute($stmt)){
+		if(mysqli_stmt_execute($stmt)){
       //      // Store result
-       //     mysqli_stmt_store_result($stmt);
-       // }
+            mysqli_stmt_store_result($stmt);
+		}
         
         // Close statement
-       // mysqli_stmt_close($stmt);
+        mysqli_stmt_close($stmt);
     //        
    // }
     /// Close connection
-   // mysqli_close($link);
+    mysqli_close($link);
        }
-	echo $_GET["floor"];
+	//echo $_GET["floor"];
 ?>
