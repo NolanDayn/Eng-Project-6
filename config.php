e5eabc967ef0bb7e29dfc8d997ceeb04fa29769a
Nolburg <nolandthomas@hotmail.ca>
2021-07-18 13:34:51 -0400

    started inserting sql into tables from site test3

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'password');
    define('DB_DATABASE', 'elevator');
-   $link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   //$link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
-   if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+   //if($link === false){
+   // die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
