74cb343d220e81a59e3dd343251237de88f722cd
Nolburg <nolandthomas@hotmail.ca>
2021-07-18 14:17:55 -0400

    started inserting sql into tables from site test

--- AddFloor.php
+++ AddFloor.php
@@ -1,7 +1,32 @@
 <?php
-require_once "config.php";
+//require_once "config.php";
 
-if($_SERVER["REQUEST_METHOD"] == "GET"){
+$db = new PDO(
+    'mysql:host=127.0.0.1;dbname=elevator',     // Database name either 'elevator' or 'joinExample'
+    'root',                                 // username
+    'password'                                // Password
+);
+
+// Boilerplate - Return arrays with keys that are the field names in the database - Call the PDO method setAttribute()
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+//if($_SERVER["REQUEST_METHOD"] == "GET"){
+
+    $query = 'INSERT INTO requests(floor) VALUES (:floor)';
+    
+    $statement = $db->prepare($query); 
+    $params = [
+        'floor' => 2
+    ];
+    $result = $statement->execute($params); 
+    var_dump($result);
+    echo '<br/><br/>';
+    $rows = $db->query("SELECT * FROM requests");
+    foreach($rows as $row) {
+        var_dump($row);
+        echo "<br/>";
+    }
+    echo '<br/><br/><br/>';
 
         //$con = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
         /*
@@ -20,29 +45,29 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
         $link->close();
 		*/
         //Insert a new requested floor to the elevators database
-        $stmt = $mysqli->prepare("'INSERT INTO `requests` (`requestNumber`, `floor`) VALUES (NULL, '?')");    
+       // $stmt = $mysqli->prepare("'INSERT INTO `requests` (`requestNumber`, `floor`) VALUES (NULL, '?')");    
 		//$stmt = mysqli_prepare($link, $sql)){
         
             // Bind variables to the prepared statement as parameters
-	    $stmt->bind_param("i", $floor);
-        $floor = _GET('floor');
+	  //  $stmt->bind_param("i", $floor);
+      //  $floor = _GET('floor');
 
         // Attempt to execute the prepared statement
 		//if(mysqli_stmt_execute($stmt)){
       //      // Store result
        //     mysqli_stmt_store_result($stmt);
 	//	}
-        $stmt->execute();
+      //  $stmt->execute();
 
-        $result = $mysqli->query('SELECT requestNumber,floor FROM requests');
-        echo $result
+      //  $result = $mysqli->query('SELECT requestNumber,floor FROM requests');
+     //   echo $result
         
         // Close statement
-        mysqli_stmt_close($stmt);
+     //   mysqli_stmt_close($stmt);
     //        
    // }
     /// Close connection
-    mysqli_close($link);
-       }
+   // mysqli_close($link);
 	//echo $_GET["floor"];
+//}
 ?>
