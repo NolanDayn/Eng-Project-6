b964299211960b9303489d6740a9abf2d01e4077
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-18 12:28:10 -0400

    Update AddFloor.php

--- AddFloor.php
+++ AddFloor.php
@@ -1,6 +1,33 @@
 <?php
-	//if($_SERVER["REQUEST_METHOD"] == "POST"){
-	//	echo $_POST["floor"];
-	//}
-	echo $_GET["floor"];
+require_once "config.php";
+
+// Processing form data when form is submitted
+$floor = $_GET["floor"];
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    // Close connection
+    mysqli_close($link);
+	//echo $_GET["floor"];
 ?>
