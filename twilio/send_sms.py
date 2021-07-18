dbd738a4133b4f3b1fe143d7c70df38c89efd5cc
RuchiPatil <ruchipatil@outlook.com>
2021-07-18 15:10:42 -0400

    Update send_sms.py

--- twilio/send_sms.py
+++ twilio/send_sms.py
@@ -1,5 +1,3 @@
-
-#!/usr/bin/env python
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
@@ -8,7 +6,7 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC46648a764504d96ef5f6ff9d7c493117'
-auth_token = 'b243eecf790651e68c3858aa1f281fc8'
+auth_token = 'e93bf8dd9e97aced2da820ba8ea48d65'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
