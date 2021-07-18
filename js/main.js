4b7608ab3afc83285fe4429ce3ef4658cd1ce186
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-18 17:55:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@ async function RequestFloor(floor){
 function PrintResults(results){
 	var endPos = results.indexOf(']') + 1;
 	//const firstTable = JSON.parse( results.substring(0,endPos) );
-	const secondTable = JSON.parse( results.substring(endPos + 1, results.length - 1) );
+	const secondTable = JSON.parse( results.substring(endPos, results.length) );
 	//FillTable(requestTable, firstTable);
 	FillTable(statusTable, secondTable);
 }
