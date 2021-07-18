ad31d580b6175c398dc9afe5cf815975c7e46fa8
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-17 21:55:52 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
    define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'your_password_here');
-   define('DB_DATABASE', 'elevatorproject');
+   define('DB_USERNAME', 'Nolan');
+   define('DB_PASSWORD', 'ESE2021');
+   define('DB_DATABASE', 'elevator');
    $link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if($link === false){
