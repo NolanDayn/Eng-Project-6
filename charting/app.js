97274ddfdcbde494ca1b2b86b00b65b10b443a25
Nolburg <nolandthomas@hotmail.ca>
2021-07-19 09:15:57 -0400

    building bar graph

--- charting/app.js
+++ charting/app.js
@@ -7,44 +7,44 @@ function showStats(){
             //( 4 means finished request and server responce is ready)
             //status 200 for 'OK'
             var resp = JSON.parse(this.responseText); //Text string returned from server in 'echo' statement
-            console.log(resp);
-
-            var date = [];
-            var requestedFloor = [];
-
-            for(var i in resp){
-                date.push("date" + resp[i].date)
-                requestedFloor.push(resp[i].requestedFloor)
-            }
-
-            console.log(requestedFloor);
-            console.log(resp[1].date)
-
-            var chartData = {
-                labels: date,
-                datasets: [
-                    {
-                        label : 'Date',
-                        backgroundColor: 'rgba(200,200,200,0.75)',
-                        borderColor: 'rgba(200,200,200,0.75)',
-                        hoverBackgroundColor: 'rgba(200,200,200,1)',
-                        hoverBorderColor: 'rgba(200,200,200,1)',
-                        data: requestedFloor
-                    }
-                ]
-            };
-
-            var ctx = document.getElementById("mycanvas");
-
-            var barGraph = new Chart(ctx, {
-                type: 'bar',
-                data: chartData
-            });
+            createBarGraph(resp);
         }
     };
 
     xmlhttpShow.open("GET", "http://142.156.193.130:50050/Eng-Project-6/charting/data.php", true);
     xmlhttpShow.send();
+}
+
+
+function createBarGraph(resp){
+    for(var i in resp){
+        date.push("Date " + resp[i].date)
+        requestedFloor.push(resp[i].requestedFloor)
+    }
+
+    console.log(requestedFloor);
+    console.log(resp[1].date)
+
+    var chartData = {
+        labels: date,
+        datasets: [
+            {
+                label : 'Date',
+                backgroundColor: 'rgba(200,200,200,0.75)',
+                borderColor: 'rgba(200,200,200,0.75)',
+                hoverBackgroundColor: 'rgba(200,200,200,1)',
+                hoverBorderColor: 'rgba(200,200,200,1)',
+            }
+        ]
+    };          data: requestedFloor
+      
+
+    var ctx = document.getElementById("mycanvas");
+
+    var barGraph = new Chart(ctx, {
+        type: 'pie',
+        data: chartData
+    });
 }
 
 showStats();
