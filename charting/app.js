34a62c21cdf4c7dcc0aeccda6eeec96e222b555f
Nolburg <nolandthomas@hotmail.ca>
2021-07-18 22:13:22 -0400

    Getting data from the database for charting

--- charting/app.js
+++ charting/app.js
@@ -1,12 +1,18 @@
-document.onload = function(){
-    $.ajax({
-        url:"http://142.156.193.130:50050/Eng-Project-6/charting/data.php",
-        method: "GET",
-        success: function(data){
-            console.log(data);
-        },
-        error: function(data){
-            console.log(data);
+function showStats(){
+
+    var xmlhttpShow = new XMLHttpRequest();
+    xmlhttpShow.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            //readyState holds the status of the XMLHttpRequest
+            //( 4 means finished request and server responce is ready)
+            //status 200 for 'OK'
+            var resp = this.responseText; //Text string returned from server in 'echo' statement
+            console.log(resp);
         }
-    });
+    };
+
+    xmlhttpShow.open("GET", "http://142.156.193.130:50050/Eng-Project-6/charting/data.php", true);
+    xmlhttpShow.send();
 }
+
+showStats();
