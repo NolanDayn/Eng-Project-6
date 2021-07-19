7225cb78771f0592a8237b47e434091f3b3afe41
Nolburg <nolandthomas@hotmail.ca>
2021-07-18 21:54:16 -0400

    Started adding data to requestedFloors database

---
+++ charting/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"http://142.156.193.130:50050/Eng-Project-6/charting/data.php",
+        method: "GET",
+        success: function(data){
+            console.log(data);
+        },
+        error: function(data){
+            console.log(data);
+        }
+    });
+});
