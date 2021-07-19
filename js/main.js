c97b09c93bcd43ebf1bbdbf38408d2c4724cc4a3
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-19 12:10:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ async function OpenDoors(open){
 	if (doorState === open) return;
 	image.style.animation = animationStrings[currentFloor * 2 + 2 + open];
 	image.style.animationFillMode = "forwards";
-	//await new Promise(function(resolve) { setTimeout(resolve, 900); });
+	await new Promise(function(resolve) { setTimeout(resolve, 900); });
 	doorState === 1 ? doorState = 0 : doorState = 1;
 
 	if (open===0){
