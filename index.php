db2d0a0ab445ba08f11354edeefa653e4753df69
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-26 13:54:14 -0400

    f

--- index.php
+++ index.php
@@ -258,13 +258,13 @@ if(isset($error) && $error != ""){
 		<div class="row">
 			<form method="get" action="./status_reports/Week_5.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" >Week 5</button></form>
 			<form method="get" action="./status_reports/Week_6.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" >Week 6</button></form>
-			<form method="get" action="./status_reports/Week_7.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" >Week 7</button></form>
-			<form method="get" action="./status_reports/Week_8.doc"><button type="submit" class="btn_status_download btn btn-success btn-lg col-sm-2" >Week 8</button></form>
+			<form method="get" action="./status_reports/Week_7.doc"><button type="submit" class="btn_status_download btn btn-danger btn-lg col-sm-2" disabled>Success Week</button></form>
+			<form method="get" action="./status_reports/Week_8.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" >Week 8</button></form>
 		</div>
 		<div class="row">
-			<form method="get" action="./status_reports/Week_9.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" disabled>Week 9</button></form>
-			<form method="get" action="./status_reports/Week_10.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" disabled>Week 10</button></form>
-			<form method="get" action="./status_reports/Week_11.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" disabled>Week 11</button></form>
+			<form method="get" action="./status_reports/Week_9.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2">Week 9</button></form>
+			<form method="get" action="./status_reports/Week_10.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2">Week 10</button></form>
+			<form method="get" action="./status_reports/Week_11.doc"><button type="submit" class="btn_status_download btn btn-success btn-lg col-sm-2">Week 11</button></form>
 			<form method="get" action="./status_reports/Week_12.doc"><button type="submit" class="btn_status_download btn btn-primary btn-lg col-sm-2" disabled>Week 12</button></form>
 		</div>
 		<div class="row">
