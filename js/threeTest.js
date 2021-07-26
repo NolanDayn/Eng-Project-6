a10d8c6c6b7001ff6b60fe06664af9f79be7392b
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-26 00:49:55 -0400

    Update threeTest.js

--- js/threeTest.js
+++ js/threeTest.js
@@ -1,4 +1,4 @@
-var canvasContainer = document.getElementByClassName("threeContainer");
+var canvasContainer = document.getElementsByClassName("threeContainer")[0];
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
