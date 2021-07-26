df49a5f241b91891c7a28e426898bb235b24d825
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-26 01:23:29 -0400

    big module

---
+++ three/src/renderers/shaders/ShaderChunk/normal_fragment_maps.glsl.js
@@ -0,0 +1,47 @@
+export default /* glsl */`
+
+#ifdef OBJECTSPACE_NORMALMAP
+
+	normal = texture2D( normalMap, vUv ).xyz * 2.0 - 1.0; // overrides both flatShading and attribute normals
+
+	#ifdef FLIP_SIDED
+
+		normal = - normal;
+
+	#endif
+
+	#ifdef DOUBLE_SIDED
+
+		normal = normal * faceDirection;
+
+	#endif
+
+	normal = normalize( normalMatrix * normal );
+
+#elif defined( TANGENTSPACE_NORMALMAP )
+
+	vec3 mapN = texture2D( normalMap, vUv ).xyz * 2.0 - 1.0;
+	mapN.xy *= normalScale;
+
+	#ifdef FLIP_NORMAL_SCALE_Y
+
+		mapN.y *= -1.0;
+
+	#endif
+
+	#ifdef USE_TANGENT
+
+		normal = normalize( vTBN * mapN );
+
+	#else
+
+		normal = perturbNormal2Arb( -vViewPosition, normal, mapN, faceDirection );
+
+	#endif
+
+#elif defined( USE_BUMPMAP )
+
+	normal = perturbNormalArb( -vViewPosition, normal, dHdxy_fwd(), faceDirection );
+
+#endif
+`;
