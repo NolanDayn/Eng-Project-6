a29dca69bb836ab85d0e82dffa6fd091d9db9e7f
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-26 01:00:17 -0400

    f

--- js/threeTest.js
+++ js/threeTest.js
@@ -1,24 +1,39 @@
 var canvasContainer = document.getElementsByClassName("threeContainer")[0];
 
+//scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+//set canvas
 const renderer = new THREE.WebGLRenderer( {canvas:document.getElementById("threeCanvas")});
 renderer.setSize( window.innerWidth, window.innerHeight );
 canvasContainer.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var model;
+//const geometry = new THREE.BoxGeometry();
+//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//const cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
+
+const loader = new GLTFLoader();
+
+loader.load( './blender_files/models.glb', function ( gltf ) {
+	model = gltf.scene;
+	scene.add( model );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 
 camera.position.z = 5;
 
 const animate = function () {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	
+	if (model) model.rotation.x += 0.01;
+	if (model) model.rotation.y += 0.01;
+	
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 };
