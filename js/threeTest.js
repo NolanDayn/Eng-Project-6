b83d0157d5e05ba4ec98a78372008fb7c15bcd79
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-26 02:55:41 -0400

    Update threeTest.js

--- js/threeTest.js
+++ js/threeTest.js
@@ -7,12 +7,17 @@ let geometry, material, mesh;
 init();
 
 function init() {
-
+	//Camera
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 	camera.position.z = 1;
-
+	
+	//Scene
 	scene = new THREE.Scene();
+	
+	const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	scene.add( light );
 
+	//Blender Model
 	const loader = new GLTFLoader();
 	loader.load( './blender_files/models.glb', function ( gltf ) {
 	mesh = gltf.scene;
@@ -30,8 +35,8 @@ function init() {
 
 function animation( time ) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	if (mesh) mesh.rotation.x = time / 2000;
+	if (mesh) mesh.rotation.y = time / 1000;
 
 	renderer.render( scene, camera );
 
