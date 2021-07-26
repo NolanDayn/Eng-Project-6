70e8518c083c34799e2babe155e53628bbade986
Nolburg <nolandthomas@hotmail.ca>
2021-07-25 22:16:53 -0400

    Improving charts and their positioning

--- index.php
+++ index.php
@@ -177,16 +177,12 @@ if(isset($error) && $error != ""){
 <div id="charts" class="jumbotron text-center">
 	<h1>Charts</h1>
 	<div id="chart-container">
-		
-		<div class="row">
-        	<canvas id="useHistory" width="1600" height="600"></canvas>
-		</div>
-		<div class="row">
-			<canvas id="startPi" width="800" height="600"></canvas>
-        	<canvas id="requestPi" width="800" height="600"></canvas>
-		</div>
+        <canvas id="useHistory" width="800" height="600"></canvas>
+		<canvas id="startPi" width="800" height="600"></canvas>
+        <canvas id="requestPi" width="800" height="600"></canvas>
     </div>
-	<button type="button" onclick="showBarGraph()">Bar Graph</button>
+	<button id="barGraphB" type="button" onclick="showBarGraph()">Bar Graph</button>
+    <button id="pieChartB" type="button" onclick="showRequestPieChart()">Pie Chart</button>
 </div>
 <div class="parallax"></div>
 
--- js/chart.js
+++ js/chart.js
@@ -112,4 +112,3 @@ function showRequestPieChart(){
     xmlhttpShow.send();
 }
 
-
