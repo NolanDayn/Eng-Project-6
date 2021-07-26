d610ef9210d28786829e4731fcd345d26cca871a
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-07-26 02:47:44 -0400

    Update threeTest.js

--- js/threeTest.js
+++ js/threeTest.js
@@ -1,5 +1,5 @@
 import * as THREE from '../three/build/three.module.js';
-import { GLTFLoader } from '../three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from '../three/examples/jsm/loaders/GLTFLoader.js';
 
 let camera, scene, renderer;
 let geometry, material, mesh;
