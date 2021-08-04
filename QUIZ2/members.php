f957e2211920e3ce10a698783bc0268b29c927f5
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-04 11:30:47 -0400

    f

---
+++ QUIZ2/members.php
@@ -0,0 +1,9 @@
+<?php
+
+<form action="/editCarNode.php">
+  <label for="fname">New Floor Number:</label><br>
+  <input type="text" id="fname" name="fname" value="1"><br>
+  <input type="submit" value="Submit">
+</form>
+
+?>
