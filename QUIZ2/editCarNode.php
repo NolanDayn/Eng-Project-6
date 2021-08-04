659f01203bcacc0d819d1593f17c5eb195dc808c
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-04 11:38:21 -0400

    f

--- QUIZ2/editCarNode.php
+++ QUIZ2/editCarNode.php
@@ -1,5 +1,27 @@
 <?php
 
-echo "hi";
+$servername = "localhost:3306";
+$username = "root";
+$password = "password";
+$dbname = "quizElevator";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE carNode SET floorNumber={$_POST['floor']} WHERE nodeID=1";
+
+if ($conn->query($sql) === TRUE) {
+	
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+
+header("location: members.php");
 
 ?>
--- QUIZ2/members.php
+++ QUIZ2/members.php
@@ -1,8 +1,8 @@
 
 
 <form action="/editCarNode.php">
-  <label for="fname">New Floor Number:</label><br>
-  <input type="text" id="fname" name="fname" value="1"><br>
+  <label for="floor">New Floor Number:</label><br>
+  <input type="text" id="floor" name="floor" value="1"><br>
   <input type="submit" value="Submit">
 </form>
 
