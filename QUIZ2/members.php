c6f1bb11db732dd2699e9cce068f8d9e181ed474
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-04 11:43:07 -0400

    Update members.php

--- QUIZ2/members.php
+++ QUIZ2/members.php
@@ -1,6 +1,6 @@
 
 
-<form action="Eng-Project-6/QUIZ2/editCarNode.php">
+<form action="editCarNode.php">
   <label for="floor">New Floor Number:</label><br>
   <input type="text" id="floor" name="floor" value="1"><br>
   <input type="submit" value="Submit">
