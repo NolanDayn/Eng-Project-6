5cbbae4ec4cc5c3a0ffdfdac799adc9cd3ab1afe
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-07 18:14:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ async function StartSabbath(){
 	sabbathOff.disabled = false;
 	sabbath = 1;
 	
-	var floor = currentFloor;
+	var floor = (JSON.parse(lastJson))[0].currentFloor;
 	var dir = 1;
 
 	for(var i = 0; i < 100; i++){
