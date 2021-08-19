f4fd92b867aee49439461bae731732acd6dc1d32
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 10:27:40 -0400

    Update registerUser.php

--- FINAL_EXAM/php/registerUser.php
+++ FINAL_EXAM/php/registerUser.php
@@ -2,12 +2,12 @@
 require_once "config.php";
 
 $login_err = "";
-$user = $_GET["username"];
-$pass = $_GET["password"];
-$email = $_GET["email"];
+$user = $_POST["username"];
+$pass = $_POST["password"];
+$email = $_POST["email"];
 
 
-if($_SERVER["REQUEST_METHOD"] == "GET"){ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){ 
 	
 	$query = "INSERT INTO credentials(username, password, email) VALUES ($user, $pass, $email)";
     $link->query($query);
