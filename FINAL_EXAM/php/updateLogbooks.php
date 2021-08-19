aeef3479fe93ed6b4fd5702d8ec1f28e73375f6a
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 14:33:55 -0400

    Update updateLogbooks.php

--- FINAL_EXAM/php/updateLogbooks.php
+++ FINAL_EXAM/php/updateLogbooks.php
@@ -14,7 +14,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			$id = $row['id'];
 		}
 	
-    $query = "INSERT INTO logbookEntries(id, date, time, text) VALUES ('$id', 'CURDATE()', 'CURTIME()', '$contents')";
+	$date = date("Y/m/d");
+	$time = time();
+    $query = "INSERT INTO logbookEntries(id, date, time, text) VALUES ('$id', '$date', '$time', '$contents')";
 	$link->query($query);	
 
 	header("location: ../logbook.php");
