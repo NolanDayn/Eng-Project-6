39402abd15ad06613a1cbc3fa389cd5ca097598d
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 14:45:31 -0400

    Create updateCredentials.php

---
+++ FINAL_EXAM/php/updateCredentials.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once "config.php";
+
+$email = $_POST["email"] == "" ? "email" : $_POST["email"];
+$password = $_POST["password"] == "" ? "password" : $_POST["password"];
+$username = $_SESSION["username"]
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){ 
+	
+	$result = "UPDATE credentials SET password = '$password', email = '$email' WHERE username = '$username'";
+	$link->query($query);
+	
+
+	header("location: ../logbook.php");
+}
+
+$link->close();
+
+?>
