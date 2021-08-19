20d2ee7756fd94ff4f399fea15a61fabbdffd9d3
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 14:50:42 -0400

    Update updateCredentials.php

--- FINAL_EXAM/php/updateCredentials.php
+++ FINAL_EXAM/php/updateCredentials.php
@@ -2,13 +2,13 @@
 session_start();
 require_once "config.php";
 
-//$email = $_POST["email"] == "" ? "email" : $_POST["email"];
-//$password = $_POST["password"] == "" ? "password" : $_POST["password"];
+$email = $_POST["email"];
+$password = $_POST["password"];
 //$username = $_SESSION["username"]
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){ 
 	
-	$result = "UPDATE credentials SET password = 'password', email = 'email' WHERE username = 'username'";
+	$result = "UPDATE credentials SET password = 'password', email = 'email' WHERE username = '$username'";
 	$link->query($query);
 	
 
