ecec991bc1339362ff631774c7724539af3a64f0
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 12:50:03 -0400

    Update guest.php

--- FINAL_EXAM/php/guest.php
+++ FINAL_EXAM/php/guest.php
@@ -1,18 +1,20 @@
 <?php
-	
-require_once "config.php";
+
 
 class Guest{
     private $username; //any INT
+	private $l;
 
     public function __construct ($_username){
 		//error checking here
         $this->username = $_username;
+		require_once "config.php";
+		$this->l = $link;
     }
 
     public function display_credentials() {
 		$u = $this->username;
-		$result = mysqli_query($link, "SELECT * FROM credentials WHERE username = '$u'");
+		$result = mysqli_query($this->l, "SELECT * FROM credentials WHERE username = '$this->username'");
 		while($row = mysqli_fetch_assoc($result))
 		{
 			echo "<h5>" . $row['id'] . "</h5>";
@@ -22,7 +24,7 @@ class Guest{
     }
 	
 	function __destruct() {
-        $link->close();
+        $this->l->close();
     }
 
 }
