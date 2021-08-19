dbb6906e991a09b4da5a655caf9d78474016e60a
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 10:41:12 -0400

    Update registerUser.php

--- FINAL_EXAM/php/registerUser.php
+++ FINAL_EXAM/php/registerUser.php
@@ -2,21 +2,21 @@
 require_once "config.php";
 
 $login_err = "";
-$user = $_POST["username"];
+$user = $_POST["userName"];
 $pass = $_POST["password"];
 $email = $_POST["email"];
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){ 
 	
-	//$result = $mysqli->query("SELECT * FROM credentials WHERE username = '$user'");
+	$result = $mysqli->query("SELECT * FROM credentials WHERE username = '$user'");
 	
-	//if($result->num_rows == 0) {	
+	if($result->num_rows == 0) {	
      	$query = "INSERT INTO credentials(username, password, email) VALUES ('$user', '$pass', '$email')";
 		$link->query($query);	
-	//} else {
-		//$login_err = "user already exists";
-	//}
+	} else {
+		$login_err = "user already exists";
+	}
 
 	header("location: ../register.php?error=$login_err");
 }
