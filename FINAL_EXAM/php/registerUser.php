e9e787552302affe63ae3899fe40bc3443c31561
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2021-08-19 10:31:07 -0400

    Update registerUser.php

--- FINAL_EXAM/php/registerUser.php
+++ FINAL_EXAM/php/registerUser.php
@@ -9,12 +9,12 @@ $email = $_POST["email"];
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){ 
 	
-	$query = "INSERT INTO credentials(username, password, email) VALUES ($user, $pass, $email)";
+	$query = "INSERT INTO credentials(username, password, email) VALUES (blah, blah, blah)";
     $link->query($query);
 
 	//header("location: ../register.php?error=$login_err");
 }
 
-
+$link->close();
 
 ?>
